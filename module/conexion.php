c530d3b86c2ac8c4adc8c8436add12a87f006d3b
YunierV <yunierv222@gmail.com>
2023-06-13 08:10:20 -0500

    conexio-V1y
    cambio en el archivo conexio

--- module/conexion.php
+++ module/conexion.php
@@ -1,6 +1,11 @@
 <?php
+$servername = "containers-us-west-188.railway.app";
+$username = "root";
+$password = "6PVYWFn7XmUboNA8phF1";
+$dbname = "railway";
+$port = "6523";
 
-$conexion = new mysqli("localhost", "root", "", "rol2");
+$conexion = new mysqli($servername, $username, $password, $dbname, $port);
 $conexion->set_charset("utf8");
 
 ?>
