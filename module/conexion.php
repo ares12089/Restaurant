70a9bf1643f6d6d875f8029069e7159308e4eba0
YunierV <yunierv222@gmail.com>
2023-06-13 10:25:50 -0500

    Revert "conexio-V1y"
    This reverts commit c530d3b86c2ac8c4adc8c8436add12a87f006d3b.

--- module/conexion.php
+++ module/conexion.php
@@ -1,11 +1,6 @@
 <?php
-$servername = "containers-us-west-188.railway.app";
-$username = "root";
-$password = "6PVYWFn7XmUboNA8phF1";
-$dbname = "railway";
-$port = "6523";
 
-$conexion = new mysqli($servername, $username, $password, $dbname, $port);
+$conexion = new mysqli("localhost", "root", "", "rol2");
 $conexion->set_charset("utf8");
 
 ?>
