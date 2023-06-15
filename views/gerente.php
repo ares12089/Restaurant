5eeb9389390210febb3871aaba3c4adedd02979f
eframhj <efrainmpriv@gmail.com>
2023-06-15 07:30:55 -0500

    css pasado a archivo separado

--- views/gerente.php
+++ views/gerente.php
@@ -33,6 +33,7 @@ $nombreBD = isset($_POST['nombre']) ? $_POST['nombre'] : '';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/3052f95fc5.js" crossorigin="anonymous"></script>
+    <link href="../css/cabecera.css" rel="stylesheet"> 
     <title>Gerente</title>
     <script>
     if (window.history.replaceState) {
