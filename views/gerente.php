b30fbe02313475cb6d0535923f029d5776ea7538
eframhj <efrainmpriv@gmail.com>
2023-06-15 08:07:54 -0500

    Revert "Update gerente.php"
    This reverts commit 7e45c44a2f9730f2271aee14c108d0fb7db42bc4.

--- views/gerente.php
+++ views/gerente.php
@@ -52,7 +52,45 @@ $nombreBD = isset($_POST['nombre']) ? $_POST['nombre'] : '';
   ?>
   <div class="container-fluid">
   <style>
+  .sidebar {
+    position: fixed;
+    right: -100cm;
+    top: 50px;
+    width: 250px;
+    height: 100vh;
+    background-color: #f9f9f9;
+    transition: right 0.3s ease;
+  }
 
+  .sidebar.open {
+    right: 0;
+  }
+
+  .sidebar ul {
+    list-style: none;
+    padding: 0;
+    margin: 20px;
+  }
+
+  .sidebar li {
+    margin-bottom: 10px;
+  }
+
+  .sidebar a {
+    text-decoration: none;
+    color: #333;
+  }
+
+  .menu-button {
+    position: fixed;
+    left: 34cm;
+    top: -1,3cm;
+    padding: 16px;
+    background-color: #f9f9f9;
+    border-radius: 50%;
+    cursor: pointer;
+    z-index: 999;
+  }
 </style>
 
 <div class="sidebar" id="sidebar">
@@ -78,11 +116,29 @@ $nombreBD = isset($_POST['nombre']) ? $_POST['nombre'] : '';
 
 <div class="text-center" style="margin-top: -120px;">
   <div class="floating-container">
-    <img src="<?php echo '../img/wallhaven-y86k3l.jpg'; ?>" alt="" class="rounded-circle img-thumbnail mx-auto" style="width: 130px; height: 130px; border: 2px solid rgb(141, 141, 141);">
+    <img src="<?php echo '../img/lol3.jpg'; ?>" alt="" class="rounded-circle img-thumbnail mx-auto" style="width: 130px; height: 130px; border: 2px solid rgb(141, 141, 141);">
   </div>
 </div>
 <style>
+@keyframes floating {
+  0% { transform: translateY(0); }
+  50% { transform: translateY(-8px); }
+  100% { transform: translateY(0); }
+}
 
+.custom-table {
+  font-size: 12px;
+}
+
+.hidden {
+  display: none;
+}
+
+
+
+.floating-container {
+  animation: floating 3s ease-in-out infinite;
+}
 </style>
 
 
@@ -173,6 +229,41 @@ $nombreBD = isset($_POST['nombre']) ? $_POST['nombre'] : '';
           }
           </script>
 
+
+          <style>
+          #modoOscuroBtn {
+          position: relative;
+          left: 31cm;
+          top: -1.1cm;
+          width: 80px;
+          height: 30px;
+          border-radius: 15px;
+          background-color: transparent;
+          transition: background-color 0.3s ease;
+          }
+
+
+          #modoOscuroBtn:focus {
+          outline: none;
+          }
+
+
+          #modoOscuroBtn:hover {
+          cursor: pointer;
+          background-color: #ccc;
+          }
+
+
+          #modoOscuroBtn:active {
+          background-color: #aaa;
+          }
+          </style>
+
+
+
+
+
+
           <div class="container-fluid row">
           <div class="col-10 p-5" method="POST" style="margin: center;">
           <div class="container">
@@ -182,11 +273,19 @@ $nombreBD = isset($_POST['nombre']) ? $_POST['nombre'] : '';
           </button>
           </div>
 
+
+
+
           <div style="position: relative; left: 19cm; top: -8px;">
           <input class="form-control-sm col-4 col-sm-3 me-2 light-table-filter" data-table="table_id" type="text" placeholder="Buscar">
           </div>
 
+
+
+
           </div>
+
+
 
     <!-- Modal Agregar -->
     <div class="modal fade" id="agregarUsuarioModal" tabindex="-1" aria-labelledby="agregarUsuarioModalLabel" aria-hidden="true">
@@ -246,73 +345,6 @@ $nombreBD = isset($_POST['nombre']) ? $_POST['nombre'] : '';
     </div>
     
     
-
-
-
-<!-- Modal Actualizar-->
-<div class="modal fade" id="modificarUsuarioModal" tabindex="-1" aria-labelledby="modificarUsuarioModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                <h3 class="text-center text-secondary"><i class="fa-solid fa-users-between-lines me-2" style="color: #000000;"></i>MODIFICAR EMPLEADO</h3>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-
-                <form class="col-12 p-5" method="POST">
-
-                 <input type="hidden" name="id" value="<?= $_GET['id'] ?>">
-              
-
-
-
-                  <div class="mb-3">
-                    <label for="nombre" class="form-label"><i class="fa-solid fa-user me-2"></i>Nombre Completo</label>
-                    <input type="text" class="form-control" id="nombre" name="nombre"> 
-                  </div>
-                  <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label"><i class="fa-solid fa-user me-2"></i>Cedula</label>
-                      <input type="text" class="form-control" id="cedula" name="cedula">
-                  </div>
-                  <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label"><i class="fa-solid fa-user me-2"></i>Usuario</label>
-                      <input type="text" class="form-control" id="usuario" name="usuario">
-                  </div>
-                  <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label"><i class="fa-solid fa-key me-2"></i>Contraseña</label>
-                      <input type="text" class="form-control" id="contraseña" name="contraseña">
-                  </div>
-                  <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label"><i class="fa-solid fa-phone me-2" style="color: #000000;"></i>Telefono</label>
-                      <input type="text" class="form-control" id="telefono" name="telefono">
-                  </div>
-                  <div class="mb-3">
-                      <label for="exampleInputEmail1" class="form-label"><i class="fa-solid fa-envelope-open-text me-2" style="color: #000000;"></i>Correo</label>
-                      <input type="text" class="form-control" id="correo" name="correo">
-                  </div>
-                  <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label"><i class="fa-solid fa-user-tag me-2"></i>Cargo</label>
-                        <select class="form-control" id="id_cargo" name="id_cargo">
-                            <?php
-                            // Consultar los cargos disponibles en la base de datos
-                            $query = "SELECT * FROM cargo";
-                            $result = mysqli_query($conexion, $query);
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo "<option value='" . $row['id'] . "'>" . $row['descripcion'] . "</option>";
-                            }
-                            ?>
-                        </select>
-                    </div>
-                
-
-                    <button type="submit" class="btn btn-warning" name="btnmodificar"><i class="fa-solid fa-pen-to-square me-2" style="color: #000000;"></i>Modificar</button>
-              </form>
-
-            
-                </div>
-            </div>
-        </div>
-    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
 
@@ -347,16 +379,24 @@ $nombreBD = isset($_POST['nombre']) ? $_POST['nombre'] : '';
                                 <td><i class="fa-solid fa-circle-user  me-2" style="color: #ffffff;"></i><?= $datos->nombre ?></td>
                                 <td><i class="fa-solid fa-id-card  me-2"></i><?= $datos->cedula ?></td>
                                 <td><i class="fa-solid fa-circle-user  me-2" style="color: #ffffff;"></i><?= $datos->usuario ?></td>
-                                <td><i class="fa-solid fa-lock  me-2" style="color: #000000;"></i><?= $datos->contraseña ?></td>
+                                <td><i class="fa-solid fa-lock  me-2" style="color: #ffffff;"></i><?= $datos->contraseña ?></td>
                                 <td><i class="fa-solid fa-phone  me-2" style="color: #ffffff;"></i><?= $datos->telefono ?></td>
                                 <td><i class="fa-solid fa-envelope me-2" style="color: #ffffff;"></i><?= $datos->correo ?></td>
                                 <td><i class="fa-solid fa-user-tag me-2"></i><?= $datos->cargo ?></td>
                                 <td>
-                                <button class="btn btn-warning btn-sm editar-btn" data-bs-toggle="modal" data-bs-target="#modificarUsuarioModal" data-id="<?php echo $row['id']; ?>">
-                                  <i class="fa-solid fa-user-edit"></i>
-                                </button>
+
+                                <!--boton actualizar -->
+                                <a href="../controller/modificar.php?id=<?= $datos->id ?>" class="btn btn-sm btn-warning"><i class="fa-solid fa-user-edit"></i></a>
+
+
+                                <!--boton eliminar-->
                                 <a href="../views/gerente.php?id=<?= $datos->id ?>" class="btn btn-sm btn-danger"><i class="fa-solid fa-user-xmark me-2"></i></a>                                </td> 
                               </tr>
+
+
+                              
+
+
                         <?php } ?>
                 </tbody>
             </table>
