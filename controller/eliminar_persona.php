b4960def5365860b7124e5a57ec10c3f0e481418
ares12089 <125369811+ares12089@users.noreply.github.com>
2023-06-17 22:25:51 -0500

    xd

--- controller/eliminar_persona.php
+++ controller/eliminar_persona.php
@@ -1,14 +1,15 @@
-<?php 
-
+<?php
 if (!empty($_GET["id"])) {
-    $id=$_GET["id"];
+    $id = $_GET["id"];
     $sql=$conexion->query(" DELETE FROM usuarios WHERE id=$id ");
-    if ($sql==1) {
+ 
+    if ($sql == 1) {
+        // Empleado eliminado con éxito
     } else {
-        echo '<div class="alert alert-danger"><i class="fa-solid fa-circle-exclamation me-2" style="color: #000000;"></i>Error Al Eliminar Persona</div>';
+        // Error al eliminar al empleado
     }
-    
 }
-
-
 ?>
+
+
+
