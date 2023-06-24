fb81c76a12409ab5469d542742562690f5805848
ares12089 <125369811+ares12089@users.noreply.github.com>
2023-06-23 21:20:43 -0500

    tiket y chef modificados

--- views/chef.php
+++ views/chef.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,6 +15,8 @@
             flex-direction: column;
             align-items: center;
             justify-content: center;
+            width: calc(100% + 100cm);
+            margin-top: 1cm;s
             border: 1px solid #ccc;
             padding: 10px;
             margin-bottom: 10px;
@@ -24,12 +26,13 @@
         .button {
             background-color: orange;
             color: white;
-            padding: 5px 10px;
+            padding: 3px 10px;
             border: none;
             cursor: pointer;
+            border-radius: 10px;
             position: relative;
             transition: transform 0.3s;
-            left: 3cm;
+            left: 6cm;
         }
 
 
@@ -37,50 +40,63 @@
         transform: scale(1.2);
         }
 
+        .custom-details {
+        width: calc(100% + 5cm);
+        margin-top: -2cm;
+        transform: scale(1.5);
+
+        }
+
+        .content {
+        text-align: left;
+        margin: 0 auto;
+        width: 80%;
+        }
+
+
 
     </style>
 </head>
 <body>
     <center>
-    <div class="container" style="margin-top: 3cm;">
-        <details>
-            <summary>Tiket 03<button class="button">Listo</button></summary>
-            <p>Hambuerguesa</p>
-            <p>Tipo: Hawaiana</p>
-            <p>Extras: Salsa inglesa</p>
-            <p>Bebidas: Gaseosa</p>
-        </details>
-    </div>
 
-    <div class="container">
-        <details>
-            <summary>Tiket 05<button class="button">Listo</button></summary>
-            <p>Hambuerguesa</p>
+    <div class="container" style="margin-top: 4cm; margin-left: -5cm;">
+    <details class="custom-details">
+        <summary>Tiket 03<button class="button">Listo</button></summary>
+        <div class="content" style="padding-left: 4cm;">
+            <p>Hamburguesa</p>
             <p>Tipo: Hawaiana</p>
             <p>Extras: Salsa inglesa</p>
             <p>Bebidas: Gaseosa</p>
-        </details>
-    </div>
+        </div>
+    </details>
+</div>
 
-    <div class="container">
-        <details>
-            <summary>Tiket 02<button class="button">Listo</button></summary>
-            <p>Hambuerguesa</p>
+<div class="container" style="margin-top: 4cm; margin-left: -5cm;">
+    <details class="custom-details">
+        <summary>Tiket 03<button class="button">Listo</button></summary>
+        <div class="content" style="padding-left: 4cm;">
+            <p>Hamburguesa</p>
             <p>Tipo: Hawaiana</p>
             <p>Extras: Salsa inglesa</p>
             <p>Bebidas: Gaseosa</p>
-        </details>
-    </div>
+        </div>
+    </details>
+</div>
 
-    <div class="container">
-        <details>
-            <summary>Tiket 01<button class="button">Listo</button></summary>
-            <p>Hambuerguesa</p>
+
+<div class="container" style="margin-top: 4cm; margin-left: -5cm;">
+    <details class="custom-details">
+        <summary>Tiket 03<button class="button">Listo</button></summary>
+        <div class="content" style="padding-left: 4cm;">
+            <p>Hamburguesa</p>
             <p>Tipo: Hawaiana</p>
             <p>Extras: Salsa inglesa</p>
             <p>Bebidas: Gaseosa</p>
-        </details>
-    </div>
+        </div>
+    </details>
+</div>
+
     </center>
 
 </body>
--- views/tiket.php
+++ views/tiket.php
@@ -1,30 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <style>
+<style>
     body {
       display: flex;
       justify-content: center;
       align-items: center;
       height: 100vh;
       background-color: black;
-      perspective: 1000px;
+      height: 100vh;
+      overflow: hidden;
     }
     
     .ticket {
-      width: 200px;
-      height: 100px;
+    
       background-color: gold;
       border-radius: 5px;
       display: flex;
       justify-content: center;
+      width: 230px; /* Aumentamos el ancho en 3 centímetros */
+      height: 130px; /* Aumentamos el alto en 3 centímetros */
       align-items: center;
-      font-family: Arial, sans-serif;
       font-size: 24px;
-      color: white;
+      color: black;
+      flex-direction: column;
+      font-size: 30px;
+      font-family: times-new-roman;
       cursor: pointer;
       transition: transform 0.3s ease;
-      transform-style: preserve-3d;
+    }
+    
+    .ticket p {
+      margin: 10px 0; /* Agregamos un margen de 10px arriba y abajo */
     }
     
     .ticket::before,
@@ -35,67 +42,111 @@
       height: 100%;
       background-color: gold;
       z-index: -1;
-      transform-origin: center;
-      transform-style: preserve-3d;
     }
     
     .ticket::before {
       left: -5px;
       width: 5px;
-      transform: skewY(-45deg);
     }
     
     .ticket::after {
       right: -5px;
       width: 5px;
-      transform: skewY(45deg);
     }
     
     .ticket:hover {
-      transform: scale(1.2) rotateX(20deg) rotateY(20deg);
+      transform: translateY(-5px);
+    }
+
+
+    .hamburger-container {
+      position: fixed;
+      top: 0;
+      left: 0;
+      pointer-events: none;
+    }
+
+    .hamburger {
+      position: absolute;
+      display: block;
+      width: 30px;
+      height: 30px;
+      border-radius: 20px;
+      background-image: url('https://img.freepik.com/vector-premium/kawaii-comida-hamburguesa-queso-vector-dibujado-mano-ilustracion-dibujos-animados-lindo-japon-anime-manga-pegatina-estilo_386815-939.jpg');
+      background-size: cover;
+      animation: fallAnimation 3s infinite linear;
+    }
+
+    @keyframes fallAnimation {
+      0% {
+        transform: translateY(-100px);
+        opacity: 0;
+      }
+      100% {
+        transform: translateY(100vh);
+        opacity: 1;
+      }
     }
   </style>
 </head>
 <body>
+<div class="hamburger-container"></div>
   <div class="ticket">
-    Ticket 01
+    <p>Ticket 01</p>
+    <p>Daniel Muñoz</p>
   </div>
 
   <script>
     const ticket = document.querySelector('.ticket');
-    const maxRotate = 20;
-    const rotateStep = 0.2;
-    let rotateX = 0;
-    let rotateY = 0;
-    let isMoving = true;
+    const maxTranslate = 6;
+    const translateStep = 0.3;
+    let translateY = 0;
+    let isFloatingUp = true;
 
-    function moveTicket() {
-      if (isMoving) {
-        rotateX += rotateStep;
-        rotateY += rotateStep;
+    function floatTicket() {
+      if (isFloatingUp) {
+        translateY -= translateStep;
 
-        if (rotateX > maxRotate) {
-          rotateX = -maxRotate;
+        if (translateY <= -maxTranslate) {
+          isFloatingUp = false;
         }
-        if (rotateY > maxRotate) {
-          rotateY = -maxRotate;
-        }
+      } else {
+        translateY += translateStep;
 
-        ticket.style.transform = `scale(1.2) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        if (translateY >= maxTranslate) {
+          isFloatingUp = true;
+        }
       }
 
-      requestAnimationFrame(moveTicket);
+      ticket.style.transform = `translateY(${translateY}px)`;
+
+      requestAnimationFrame(floatTicket);
     }
 
-    moveTicket();
-
-    ticket.addEventListener('mouseenter', () => {
-      isMoving = false;
-    });
-
-    ticket.addEventListener('mouseleave', () => {
-      isMoving = true;
-    });
+    floatTicket();
   </script>
+
+<script>
+    function createHamburger() {
+      const hamburgerContainer = document.querySelector('.hamburger-container');
+      const hamburger = document.createElement('div');
+      hamburger.classList.add('hamburger');
+      hamburger.style.left = Math.random() * 100 + 'vw';
+      hamburger.style.animationDuration = Math.random() * 3 + 2 + 's';
+      hamburgerContainer.appendChild(hamburger);
+
+      setTimeout(() => {
+        hamburger.remove();
+      }, 5000);
+    }
+
+    function startRainingHamburgers() {
+      setInterval(createHamburger, 200);
+    }
+
+    startRainingHamburgers();
+  </script>
+
+
 </body>
 </html>
